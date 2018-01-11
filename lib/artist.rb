1b4f3890e74f92c764cb70f3e59f90a2917a4164
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-11 21:33:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,16 @@
 class Artist
-  attr_accessor :name, :songs, :save
+  attr_accessor :name, :songs
 
   @@all = []
 
   def initialize(name)
     @name = name
+    
   end
 
   def add_song(title)
     new_song = Song.new(title)
     new_song.artist = self
-  end
-
-  def self.save
-    @@all << self
   end
 
 end
