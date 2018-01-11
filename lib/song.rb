8bd7999437cf0fe360c12d6e53a9c792aae51cdd
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-11 22:14:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,7 @@ class Song
   def self.new_by_filename(filename)
     file = filename.split(" - ")
     new_song = self.new(file[1])
+    new_artist = Artist.new(new_song)
     new_song.artist.name = file[0]
   end
 end
