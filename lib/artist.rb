e36451286b5821216b068bc845a1926a20ecc3ae
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-11 21:55:14 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,8 +14,8 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    @@<< self.new(name) unless self.all.detect { |artist| artist.name == name }
-    
+    @@all << self.new(name) unless self.all.detect { |artist| artist.name == name }
+
   end
 
   def save
