718edaff85eda119ac40d26f9b10f89afc34ed15
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-11 21:48:13 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,6 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
   end
 
   def add_song(song)
@@ -15,6 +14,10 @@ class Artist
 
   def find_or_create_by_name(name)
 
+  end
+
+  def save
+    @@all << self
   end
 
   def self.all
